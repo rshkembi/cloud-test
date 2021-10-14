c5b09ac6b589e2e04cd59108233cd4a666621c53
kgardner-he <kgardner@human-element.com>
2021-10-14 09:34:13 -0500

    update: merging .gitignore changes from develop

--- dev/tests/integration/testsuite/Magento/CustomerBalance/_files/creditmemo_with_customer_balance_rollback.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-use Magento\TestFramework\Workaround\Override\Fixture\Resolver;
-
-Resolver::getInstance()->requireDataFixture('Magento/CustomerBalance/_files/order_with_customer_balance_rollback.php');
--- dev/tests/integration/testsuite/Magento/GiftCard/_files/customer_invoice_with_gift_card_rollback.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-use Magento\TestFramework\Workaround\Override\Fixture\Resolver;
-
-Resolver::getInstance()->requireDataFixture('Magento/GiftCard/_files/customer_order_with_gift_card_rollback.php');
