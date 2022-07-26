16922a592c1022447cfa3ac7a5794c1fcf442fbf
hiddenmike <merickson@human-element.com>
2022-07-26 18:15:52 -0400

    [STOE1-100] Created a mixin to add scroll to top behavior when error messages are added to the page via the messages UI component.

---
+++ app/design/frontend/HumanElement/stover_default/Magento_Ui/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Ui/js/model/messages': {
+                'Magento_Ui/js/model/messages-mixin': true
+            }
+        }
+    }
+};
