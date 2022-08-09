3e62c548941014475baff47990c686513b316410
Primary Webuser <ispaho@human-element.com>
2022-08-09 20:35:30 +0000

    STOE1-35 - optimize description attribute

--- app/code/HumanElement/DescriptionAttributeOptimization/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'HumanElement_DescriptionAttributeOptimization',
-    __DIR__
-);
