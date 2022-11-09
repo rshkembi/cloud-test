67eb5de7397d1f1cbed923d1ed783a03f7f365c7
Primary Webuser <ispaho@human-element.com>
2022-11-09 16:30:23 +0000

    STOE1-167 - fix review link on PDP

---
+++ app/design/frontend/HumanElement/stover_default/Yotpo_Yotpo/web/js/review.js
@@ -0,0 +1,18 @@
+define([
+    'jquery',
+    'jquery-ui-modules/widget'
+], function ($) {
+    'use strict';
+
+    $.widget('mage.productReview', {
+
+        /** @inheritdoc */
+        _create: function () {
+            $(".yotpo.bottomLine").on('click' , function() {
+                $("#tab-label-yotpo_widget_div").trigger('click');
+            });
+        }
+    });
+
+    return $.mage.productReview;
+});
