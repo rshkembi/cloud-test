25f4684713b01d03fbadc322678b8c634e83e86b
Rafael Shkembi <rshkembi@human-element.com>
2023-11-16 17:05:26 +0000

    Added preorder module

---
+++ app/code/Stover/PreOrder/Setup/Patch/Data/AddPreOrderButton.php
@@ -0,0 +1,87 @@
+<?php
+namespace Stover\PreOrder\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Eav\Model\Entity\Attribute\Source\Boolean;
+
+/**
+ * Class AddProductIdAttribute
+ */
+class AddPreOrderButton implements DataPatchInterface
+{
+    public const ATTRIBUTE_CODE = 'pre_order';
+    public const ATTRIBUTE_LABEL = 'Pre Order Status';
+
+    /** @var ModuleDataSetupInterface */
+    private ModuleDataSetupInterface $moduleDataSetup;
+
+    /** @var EavSetupFactory */
+    private EavSetupFactory $eavSetupFactory;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            static::ATTRIBUTE_CODE,
+            [
+                'type' => 'int',
+                'backend' => '',
+                'frontend' => '',
+                'label' => __(static::ATTRIBUTE_LABEL),
+                'input' => 'boolean',
+                'source' => Boolean::class,
+                'class' => '',
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => true,
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => false,
+                'unique' => false,
+                'apply_to' => '',
+                'attribute_set' => 'Default',
+                'group' => '',
+                'default' => '0'
+            ]
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
